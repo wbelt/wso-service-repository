9a541724608ad0720c0afc674a45424f068c5d30
Wes Belt <wes@diydev.io>
2022-06-18 21:48:32 -0400

    initial db support

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, send_file, url_for, send_from_directory
+from db import provide_db_services_c
 
 app = Flask(__name__, template_folder="templates")
 
@@ -14,16 +15,10 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static\images'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
+@app.route('/hello')
 def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+   print('Request for hello page received')
+   return render_template('hello.html', name = countServices())
 
 @app.route("/css/<path:path>")
 def cssFileRoute(path):
@@ -44,6 +39,14 @@ def assetsFileRoute(path):
 def templateFileRoute(path):
    print('Request for templateFileRoute received')
    return render_template(path)
+
+@provide_db_services_c
+def countServices(c):
+   items = list(c.query_items(
+      query="SELECT VALUE COUNT(1) FROM c",
+      enable_cross_partition_query=True
+   ))
+   return items[0]
 
 if __name__ == '__main__':
    app.run()
---
+++ db.py
@@ -0,0 +1,61 @@
+
+from azure.cosmos import exceptions, CosmosClient, PartitionKey
+from functools import wraps
+import os, family
+from requests import request
+
+endpoint = "https://diydev-control-plane-db.documents.azure.com:443/"
+
+
+def provide_db_client(func):
+    @wraps(func)
+    def new_function(*args, **kwargs):
+        client = CosmosClient(endpoint, os.environ.get('dbSECRETkey'))
+        return func(client, *args, **kwargs)
+    return new_function
+
+def provide_db_service_repository(func):
+    @wraps(func)
+    @provide_db_client
+    def new_function(client, *args, **kwargs):
+        db = client.get_database_client("service-repository")
+        return func(db, *args, **kwargs)
+    return new_function
+
+def provide_db_services_c(func):
+    @wraps(func)
+    @provide_db_service_repository
+    def new_function(db, *args, **kwargs):
+        c = db.get_container_client("services")
+        return func(c, *args, **kwargs)
+    return new_function
+
+@provide_db_client
+def list_databases(client):
+    databases = list(client.list_databases())
+    if not databases:
+        return
+    for database in databases:
+        print(database['id'])
+
+@provide_db_service_repository
+def list_containers(db):
+    containers = list(db.list_containers())
+    if not containers:
+        return
+    for container in containers:
+        print(container['id'])
+
+@provide_db_services_c
+def list_items(c):
+    items = list(c.query_items(
+        query="SELECT * FROM c",
+        enable_cross_partition_query=True
+    ))
+    if not items:
+        return
+    for item in items:
+        print(item['id'])
+
+if __name__ == '__main__':
+   print(list_containers())
